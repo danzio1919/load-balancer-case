299abe661ac72292adf188c6df0bd66b0accc903
danzio1919 <danzio1919@gmail.com>
2026-06-23 22:44:28 +0300

    feat: updates simulation engine package to include new strategies

--- backend/simulation_engine/__init__.py
+++ backend/simulation_engine/__init__.py
@@ -1,6 +1,11 @@
 from simulation_engine.models import Request, Server
 from simulation_engine.load_balancer import LoadBalancer
-from simulation_engine.strategies import LeastLoadedMemoryStrategy, SchedulingStrategy
+from simulation_engine.strategies import (
+    SchedulingStrategy,
+    LeastLoadedMemoryStrategy,
+    RoundRobinStrategy,
+    BestFitStrategy,
+)
 from simulation_engine.engine import SimulationEngine
 
 __all__ = [
@@ -9,5 +14,7 @@ __all__ = [
     "LoadBalancer",
     "SchedulingStrategy",
     "LeastLoadedMemoryStrategy",
+    "RoundRobinStrategy",
+    "BestFitStrategy",
     "SimulationEngine",
 ]
